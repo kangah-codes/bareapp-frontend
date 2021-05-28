d2ebe611ee0daaa0d869bd8d81b199cc06e21f43
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-05-28 23:34:17 +0000

    refactor: Moved children prop to within navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,8 +3,7 @@ import Navbar from "./Nav";
 export default function Layout({ title = "Bareapp", children }) {
 	return (
 		<div>
-			<Navbar />
-			<div>{children}</div>
+			<Navbar children={children} />
 		</div>
 	);
 }
