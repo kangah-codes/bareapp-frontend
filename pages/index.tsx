ca80bcf6054b5c7a85206a0567396998e7005a56
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-05-28 23:34:52 +0000

    refactor: Added dummy cards to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
+import ImgCard from "../components/ImgCard";
+
+const Images = () => {
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+			{[...Array(10).keys()].map((_items) => (
+				<ImgCard />
+			))}
+		</div>
+	);
+};
 
 export default function Home() {
-	return (
-		<Layout>
-			<p>LOL</p>
-		</Layout>
-	);
+	return <Layout children={<Images />} />;
 }
