5fa0ae713e3b676bb4220c0b7d610e6c1a9e1754
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-06-01 21:03:34 +0000

    refactor: Added title and active props

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,9 @@
 import Navbar from "./Nav";
 
-export default function Layout({ title = "Bareapp", children }) {
+export default function Layout({ title = "Bareapp", children, active }) {
 	return (
 		<div>
-			<Navbar children={children} />
+			<Navbar children={children} active={active} title={title} />
 		</div>
 	);
 }
